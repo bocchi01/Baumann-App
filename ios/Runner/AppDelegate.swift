f139a548c79d4a91cdd01e096cf87478379de068
Tommaso <tommytattaaa@gmail.com>
2025-10-03 13:44:09 +0200

    fix ios

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,24 +1,52 @@
 #if canImport(UIKit)
 import UIKit
 import Flutter
+import FirebaseCore
+
 @UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
+  override init() {
+    super.init()
+    configureFirebaseIfNeeded()
+  }
+
   override func application(
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
+    configureFirebaseIfNeeded()
+    
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
+
+  private func configureFirebaseIfNeeded() {
+    guard FirebaseApp.app() == nil else { return }
+    FirebaseApp.configure()
+  }
 }
+
 #elseif canImport(FlutterMacOS)
 import FlutterMacOS
+import FirebaseCore // <-- Aggiunto l'import mancante
 
 @main
 class AppDelegate: FlutterAppDelegate {
+  override init() {
+    super.init()
+    configureFirebaseIfNeeded()
+  }
+
   override func applicationDidFinishLaunching(_ notification: Notification) {
+    configureFirebaseIfNeeded()
+    
     RegisterGeneratedPlugins(registry: self)
     super.applicationDidFinishLaunching(notification)
+  }
+
+  private func configureFirebaseIfNeeded() {
+    guard FirebaseApp.app() == nil else { return }
+    FirebaseApp.configure()
   }
 }
 #endif
